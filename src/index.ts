7785a7b870d4e060da0b69ffe4c55fba5f0d73af
ambientlight <primary.taras.vozniuk@gmail.com>
2022-05-09 00:39:58 +0800

    enable clipboard and bootstrap dhcp when network_relay_url is passed

--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,7 @@ window.onresize = (event: UIEvent) => fit.fit();
 const params = (new URL(window.location.toString())).searchParams;
 const networkRelayURLParam = params.get("network_relay_url");
 const networkRelayURL = networkRelayURLParam ? decodeURIComponent(networkRelayURLParam) : undefined;
+let relayDidBootstrap = false;
 
 const emulator = new V86Starter({
   wasm_path: "/v86.wasm",
@@ -85,7 +86,29 @@ const initialSequence = () => {
   sendCode([13]);
 }
 
+const relayInit = () => {
+  const enableDhcp = "ip link set enp0s5 down && rmmod ne2k-pci && modprobe ne2k-pci && dhcpcd -w4 enp0s5";
+  sendString(enableDhcp);
+  sendCode([13]);
+}
+
 terminal.writeln("Loading... ");
 terminal.onData(sendString);
+terminal.attachCustomKeyEventHandler((key: KeyboardEvent) => {
+  if (key.code === 'KeyV' && key.ctrlKey && key.shiftKey) {
+    navigator.clipboard.readText().then(clipText => terminal.write(clipText));
+    return false;
+  }
+  
+  return true;
+});
+
 emulator.add_listener("emulator-ready", () => window.setTimeout(() => initialSequence(), 100));
 emulator.add_listener("serial0-output-char", (chr: string) => terminal.write(chr));
+// side-effects only
+emulator.add_listener("serial0-output-line", (line: string) => {
+  if(line.startsWith("Last login:") && !relayDidBootstrap && networkRelayURL){
+    relayInit();
+    relayDidBootstrap = true;
+  }
+});
